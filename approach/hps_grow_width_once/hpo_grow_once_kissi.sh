742a84e77bcd479f55bbd417c41ef662ae769698
Lukas Fehring <lukas.fehring@stud.uni-hannover.de>
2024-07-16 10:14:56 +0200

    Add first version of grow_width_once

---
+++ approach/hps_grow_width_once/hpo_grow_once_kissi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=grow_once
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16GB
+#SBATCH --time=72:00:00
+
+# Navigate to the project directory
+cd /mnt/home/lfehring/MasterThesis/architectures-in-rl
+module load Miniconda3
+conda activate rl-architecture
+export PYTHONPATH="$PYTHONPATH:/mnt/home/lfehring/MasterThesis/architectures-in-rl"
+# Run the Python script with specified module
+
+# Run the Python script with specified module
+python approach/hps_grow_depth_once/hpo_grow_once.py -m 

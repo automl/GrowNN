5f604318e1ed862b9d7463528d6aa7b9100dcf26
Lukas Fehring <l.fehring@ai.uni-hannover.de>
2024-07-29 13:39:47 +0200

    Update configurations

--- baselines/blackbox_only_hpo_1_layer/blackbox_only_hpo_kissi.sh
+++ baselines/blackbox_only_hpo_1_layer/blackbox_only_hpo_kissi.sh
@@ -11,4 +11,4 @@ module load Miniconda3
 conda activate rl-architecture
 export PYTHONPATH="$PYTHONPATH:/mnt/home/lfehring/MasterThesis/architectures-in-rl"
 # Run the Python script with specified module
-python baselines/blackbox_only_hpo/blackbox_only_hpo.py -m
+python baselines/blackbox_only_hpo_1_layer/blackbox_only_hpo.py -m

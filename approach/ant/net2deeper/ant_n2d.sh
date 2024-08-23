8dba5cee2bc4a062ea76a963896a7e34053e89b0
Lukas Fehring <l.fehring@ai.uni-hannover.de>
2024-08-23 10:40:26 +0200

    Update Net2Deeper Run script

--- approach/ant/net2deeper/ant_n2d.sh
+++ approach/ant/net2deeper/ant_n2d.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=rl_training
+#SBATCH --job-name=ant_n2d
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=8GB
 #SBATCH --time=72:00:00
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
 
 # Navigate to the project directory
 cd /mnt/home/lfehring/MasterThesis/architectures-in-rl

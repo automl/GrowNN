8c84ec1f063a10f3077ba5ebefdd84ddaa3eb6f0
Lukas Fehring <l.fehring@ai.uni-hannover.de>
2024-09-10 15:33:02 +0200

    Update execution files

---
+++ baselines/minihack/increase_difficulty_1_layers/blackbox_only_hpo_kissi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=baseline_increase_difficulty
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8GB
+#SBATCH --time=72:00:00
+
+# Navigate to the project directory
+cd /mnt/home/lfehring/MasterThesis/architectures-in-rl
+module load Miniconda3
+conda activate rl-architecture
+export PYTHONPATH="$PYTHONPATH:/mnt/home/lfehring/MasterThesis/architectures-in-rl"
+# Run the Python script with specified module
+python baselines/minihack/increase_difficulty_1_layers/blackbox_only_hpo.py -m

1ffd374a034daa8e3b936896a878ea155b248c5a
Lukas Fehring <lukasfehring@gmail.com>
2025-01-30 11:33:02 +0100

    Update weight pots

--- smac3_output/sync_files.sh
+++ smac3_output/sync_files.sh
@@ -8,10 +8,11 @@ LOCAL_DIR="."  # Replace with your local directory
 SSH_KEY="~/.ssh/id_rsa"  # Path to your RSA key
 
 # Rsync command
-rsync -avz -e "ssh -i ${SSH_KEY}" ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR} ${LOCAL_DIR}
+rsync -vz -e "ssh -i ${SSH_KEY}" ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR} ${LOCAL_DIR}
 
 # Explanation of options:
 # -a: Archive mode (recursive copy and preserves attributes)
 # -v: Verbose mode
 # -z: Compress file data during the transfer
 # -e: Specifies the remote shell to use
+    
